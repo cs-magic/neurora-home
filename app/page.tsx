e277c51a804fceb2e8e0367424e190c6d169ebf9
markshawn2020 <shawninjuly@gmail.com>
2024-10-06 00:11:09 +0800

    :sparkles: added neurora-banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { NeuroraBannerSVG } from "@assets/branding/neurora/neurora-banner.svg.ts";
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
@@ -28,16 +29,25 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        <p>NEURORA</p>
-        {/*<p className={"text-6xl"}>飞脑科技</p>*/}
-      </h1>
+
+      <NeuroraBannerSVG className="py-3.5 px-1 w-full max-w-screen-md z-10 cursor-default duration-100 animate-title text-white" />
+      {/*<h1 className="py-3.5 px-0.5 z-10 cursor-default duration-100 animate-title text-4xl text-transparent bg-white text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">*/}
+      {/*  <p>NEURORA</p>*/}
+      {/*  /!*<p className={"text-6xl"}>飞脑科技</p>*!/*/}
+      {/*</h1>*/}
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-      <h2 className="text-sm text-zinc-500 ">
-          Hi, I'm <Link href="https://github.com/markshawn2020" target="_blank" className="underline duration-500 hover:text-zinc-300">Mark</Link>, 
-          An Indie Hacker passionate about building cooool things 🚀 🚀 🚀
+        <h2 className="text-sm text-zinc-500 ">
+          Hi, I'm{" "}
+          <Link
+            href="https://github.com/markshawn2020"
+            target="_blank"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            Mark
+          </Link>
+          , An Indie Hacker passionate about building cooool things 🚀 🚀 🚀
         </h2>
       </div>
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,24 @@ import dotenv from "dotenv";
 import { fileURLToPath } from "node:url";
 
 dotenv.config({
-	path: path.join(fileURLToPath(import.meta.url), "../../.env"),
+  path: path.join(fileURLToPath(import.meta.url), "../../.env"),
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default withContentlayer(nextConfig);
